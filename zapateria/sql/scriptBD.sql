303a714e48d1c54cd395c59e779e8dbb1a026b51
sebasx3601 <sebastianx3600@gmail.com>
2021-10-02 19:44:34 -0300

    La primera version del script sql

--- zapateria/sql/scriptBD.sql
+++ zapateria/sql/scriptBD.sql
@@ -1,9 +1,245 @@
+drop database if exists `zapateria`;
 CREATE DATABASE `zapateria`;
 USE zapateria;
-CREATE TABLE `producto`
+
+CREATE TABLE `clientes`
+(
+  `IdCliente` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellido` varchar(45) NOT NULL,
+  `CorreoElectronico` varchar(45) NOT NULL,
+  `LimiteCredito` int(11) NOT NULL,
+  `CreditoDisponible` int(11) NOT NULL,
+  `TipoCliente` varchar(45) NOT NULL,
+  `ImpuestoAFIP` varchar(45) NOT NULL,
+  `Estado` varchar(45) NOT NULL,
+  `Calle` varchar(45) NOT NULL,
+  `Altura` varchar(45) NOT NULL,
+  `Pais` varchar(45) NOT NULL,
+  `Provincia` varchar(45) NOT NULL,
+  `Localidad` varchar(45) NOT NULL,
+  `CodPostal` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdCliente`)
+);
+CREATE TABLE `empleados`
+(
+  `IdEmpleado` int(11) NOT NULL AUTO_INCREMENT,
+  `CUIL` varchar(45) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellido` varchar(45) NOT NULL,
+  `CorreoElectronico` varchar(45) NOT NULL,
+  `TipoEmpleado` varchar(45) NOT NULL,
+  `Contraseña` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdEmpleado`)
+);
+
+CREATE TABLE `movimientosCaja`
+(
+  `IdMovimientosCaja` int(11) NOT NULL AUTO_INCREMENT,
+  `IdSucursal` int(11) NOT NULL,
+  `Fecha` DATE NOT NULL,
+  `Detalle` varchar(45) NOT NULL,
+  `TipoDeMovimiento` varchar(45) NOT NULL,
+  `Monto` int(11) NOT NULL,
+  `CodMoneda` int(11) NOT NULL,
+  `Monto_Convertido` int(11) NOT NULL,
+  `IdFactura` int(11) NOT NULL,
+  `IdCierre` int(11) NOT NULL,
+  `IdEmpleado` int(11) NOT NULL,
+  PRIMARY KEY (`IdMovimientosCaja`)
+);
+
+CREATE TABLE `Factura(venta)`
+(
+  `IdFactura` int(11) NOT NULL AUTO_INCREMENT,
+  `MontoPendiente` int(11) NOT NULL,
+  `Detalle` varchar(45) NOT NULL,
+  `IdCliente` int(11) NOT NULL,
+  `IdEmpleado` int(11) NOT NULL,
+  `Fecha` DATE NOT NULL,
+  
+  `TipoFactura` varchar(45) NOT NULL,
+  `NroFacturaCompleta` varchar(45) NOT NULL,
+  `IdSucursal` int(11) NOT NULL,
+  `TerceraParte` int(11) NOT NULL,
+  
+  `Descuento` int(11) NOT NULL,
+  `TotalFactura` int(11) NOT NULL,
+  `IdMedioDePago` int(11) NOT NULL,
+  `TipoVenta` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdFactura`)
+);
+
+CREATE TABLE `cajaSucursal`
+(
+  `IdSucursal` int(11) NOT NULL AUTO_INCREMENT,
+  `IdMoneda` int(11) NOT NULL,
+  `CantidadEnCaja` int(11) NOT NULL,
+  PRIMARY KEY (`IdSucursal`)
+);
+
+CREATE TABLE `detalle`
+(
+  `IdDetalle` int(11) NOT NULL AUTO_INCREMENT,
+  `IdProducto` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Descripcion` varchar(45) NOT NULL,
+  `PrecioCosto` int(11) NOT NULL,
+  `PrecioVenta` int(11) NOT NULL,
+  `Monto` int(11) NOT NULL,
+  PRIMARY KEY (`IdDetalle`)
+);
+
+CREATE TABLE `valorMoneda`
+(
+  `IdMoneda` int(11) NOT NULL AUTO_INCREMENT,
+  `Descripcion` varchar(45) NOT NULL,
+  `TasaConversion` int(11) NOT NULL,
+  PRIMARY KEY (`IdMoneda`)
+);
+
+CREATE TABLE `medioPago`
+(
+  `IdMedioDePago` int(11) NOT NULL AUTO_INCREMENT,
+  `MetodoDePago` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdMedioDePago`)
+);
+
+CREATE TABLE `HistorialCambioMoneda`
+(
+  `IdCambioMoneda` int(11) NOT NULL AUTO_INCREMENT,
+  `IdMoneda` int(11) NOT NULL,
+  `IdEmpleado` int(11) NOT NULL,
+  `TasaAnterior` int(11) NOT NULL,
+  `NuevaTasa` int(11) NOT NULL,
+  `Fecha` DATE NOT NULL,
+  PRIMARY KEY (`IdCambioMoneda`)
+);
+
+CREATE TABLE `CierreCaja`
+(
+  `IdCierre` int(11) NOT NULL AUTO_INCREMENT,
+  `IdSucursal` int(11) NOT NULL,
+  `FechaCierre` DATE NOT NULL,
+  `SaldoTotal` int(11) NOT NULL,
+  PRIMARY KEY (`IdCierre`)
+);
+
+CREATE TABLE `Stock`
+(
+  `IdStock` int(11) NOT NULL AUTO_INCREMENT,
+  `IdSucursal` int(11) NOT NULL,
+  `IdProducto` int(11) NOT NULL,
+  `CodigoLote` varchar(45) NOT NULL,
+  `StockDisponible` int(11) NOT NULL,
+  PRIMARY KEY (`IdStock`)
+);
+
+CREATE TABLE `Sucursales`
+(
+  `IdSucursal` int(11) NOT NULL AUTO_INCREMENT,
+  `Telefono` varchar(45) NOT NULL,
+  `Calle` varchar(45) NOT NULL,
+  `Altura` varchar(45) NOT NULL,
+  `Provincia` varchar(45) NOT NULL,
+  `Localidad` varchar(45) NOT NULL,
+  `Pais` varchar(45) NOT NULL,
+  `CodigoPostal` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdSucursal`)
+);
+
+CREATE TABLE `MaestroProductos`
+(
+  `IdMaestroProducto` int(11) NOT NULL AUTO_INCREMENT,
+  `Descripcion` varchar(45) NOT NULL,
+  `Tipo` varchar(45) NOT NULL,
+  `Fabricado` varchar(45) NOT NULL,
+  `PrecioCosto` int(11) NOT NULL,
+  `PrecioVenta` int(11) NOT NULL,
+  `PuntoRepositorio` int(11) NOT NULL,
+  `IdProveedor` int(11) NOT NULL,
+  `Talle` varchar(45) NOT NULL,
+  `UnidadMedida` int(11) NOT NULL,
+  `Estado` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdMaestroProducto`)
+);
+
+CREATE TABLE `Recetas`
+(
+  `IdReceta` int(11) NOT NULL AUTO_INCREMENT,
+  `IdProducto` int(11) NOT NULL,
+  `Descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdReceta`)
+);
+
+CREATE TABLE `Proveedor`
+(
+  `Idproveedor` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `CorreoElectronico` varchar(45) NOT NULL,
+  `LimiteCredito` int(11) NOT NULL,
+  `CreditoDisponible` int(11) NOT NULL,
+  PRIMARY KEY (`Idproveedor`)
+);
+
+CREATE TABLE `PasosReceta`
+(
+  `IdPasoReceta` int(11) NOT NULL AUTO_INCREMENT,
+  `IdReceta` int(11) NOT NULL,
+  `NroOrden` int(11) NOT NULL,
+  `IdPaso` int(11) NOT NULL,
+  PRIMARY KEY (`IdPasoReceta`)
+);
+
+CREATE TABLE `Paso`
+(
+  `IdPaso` int(11) NOT NULL AUTO_INCREMENT,
+  `Descripción` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdPaso`)
+);
+
+CREATE TABLE `MaterialesDePaso`
+(
+  `IdMaterialDePaso` int(11) NOT NULL AUTO_INCREMENT,
+  `IdMaterial` int(11) NOT NULL,
+  `CantidadUsada` int(11) NOT NULL,
+  `IdPaso` int(11) NOT NULL,
+  PRIMARY KEY (`IdMaterialDePaso`)
+);
+
+CREATE TABLE `OrdenFabrica`
+(
+  `IdOrdenFabrica` int(11) NOT NULL AUTO_INCREMENT,
+  `IdProd` int(11) NOT NULL,
+  `FechaRequerido` DATE NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `CodigoLote` varchar(45) NOT NULL,
+  PRIMARY KEY (`IdOrdenFabrica`)
+);
+
+CREATE TABLE `FabricacionesEnMarcha`
+(
+  `IdOrdenFabrica` int(11) NOT NULL AUTO_INCREMENT,
+  `IdReceta` int(11) NOT NULL,
+  `NroPasoActual` int(11) NOT NULL,
+  PRIMARY KEY (`IdOrdenFabrica`)
+);
+
+
+
+
+
+
+
+
+
+CREATE TABLE `estadoMaterialFabricacion`
 (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
-  `idProveedor` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 );
+insert into estadoMaterialFabricacion values(1,'activo');
+insert into estadoMaterialFabricacion values(2,'inactivo');
+insert into estadoMaterialFabricacion values(3,'suspendido');
+insert into estadoMaterialFabricacion values(4,'desactualizado');
