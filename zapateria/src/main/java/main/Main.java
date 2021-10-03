6b2ed8f51518c5ae7f6df73fe19fba6931f9534c
sebasx3601 <sebastianx3600@gmail.com>
2021-10-02 23:02:22 -0300

    Se borraron los elementos iniciales de ejemplo
    Tras hablarlo se decidio borrar los elementos de ejemplo

--- zapateria/src/main/java/main/Main.java
+++ zapateria/src/main/java/main/Main.java
@@ -1,16 +1,8 @@
 package main;
 
-import modelo.Zapateria;
-import persistencia.dao.mysql.DAOSQLFactory;
-import presentacion.controlador.Controlador;
-import presentacion.vista.Vista;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Vista vista = new Vista();
-		Zapateria modelo = new Zapateria(new DAOSQLFactory());
-		Controlador controlador = new Controlador(vista, modelo);
-		controlador.inicializar();
+		
 	}
 }
--- zapateria/src/main/java/modelo/Zapateria.java
+++ zapateria/src/main/java/modelo/Zapateria.java
@@ -1,27 +1,8 @@
 package modelo;
 
 import java.util.List;
-import dto.ProductoDTO;
 import persistencia.dao.interfaz.DAOAbstractFactory;
-import persistencia.dao.interfaz.ProductoDAO;
 
 public class Zapateria {
-	private ProductoDAO producto;
-
-	public Zapateria(DAOAbstractFactory metodo_persistencia) {
-		this.producto = metodo_persistencia.createProductoDAO();
-	}
-
-	public void agregarProducto(ProductoDTO nuevoProducto) {
-		this.producto.insert(nuevoProducto);
-	}
-
-	public void borrarProducto(ProductoDTO productoEliminar) {
-		this.producto.delete(productoEliminar);
-	}
-
-	public List<ProductoDTO> obtenerProducto() {
-		return this.producto.readAll();
-	}
 
 }
