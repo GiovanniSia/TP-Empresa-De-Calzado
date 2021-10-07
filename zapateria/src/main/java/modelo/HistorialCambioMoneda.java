57398f4445f1c10d3bc807d52764605c2c345b57
GiovanniSia <76817363+GiovanniSia@users.noreply.github.com>
2021-10-07 19:12:36 -0300

    Agrega modelo de historialCambioMoneda

---
+++ zapateria/src/main/java/modelo/HistorialCambioMoneda.java
@@ -0,0 +1,36 @@
+package modelo;
+
+import java.util.List;
+
+import dto.HistorialCambioMonedaDTO;
+import persistencia.dao.interfaz.DAOAbstractFactory;
+import persistencia.dao.interfaz.HistorialCambioMonedaDAO;
+
+public class HistorialCambioMoneda {
+	private HistorialCambioMonedaDAO historialCambioMoneda;
+
+	public HistorialCambioMoneda(DAOAbstractFactory metodo_persistencia) {
+		this.historialCambioMoneda = metodo_persistencia.createHistorialCambioMonedaDAO();
+	}
+
+	public void insert(HistorialCambioMonedaDTO historialCambioMoneda) {
+		this.historialCambioMoneda.insert(historialCambioMoneda);
+	}
+
+	public void delete(HistorialCambioMonedaDTO historialCambioMoneda_a_eliminar) {
+		this.historialCambioMoneda.delete(historialCambioMoneda_a_eliminar);
+	}
+
+	public void update(int id_historialCambioMoneda_a_actualizar, HistorialCambioMonedaDTO historialCambioMoneda_nuevo) {
+		this.historialCambioMoneda.update(id_historialCambioMoneda_a_actualizar, historialCambioMoneda_nuevo);
+	}
+
+	public List<HistorialCambioMonedaDTO> readAll() {
+		return this.historialCambioMoneda.readAll();
+	}
+
+	public List<HistorialCambioMonedaDTO> getMaestroProductoAproximado(String nombreColumna1, String txtAprox1,
+			String nombreColumna2, String txtAprox2) {
+		return this.historialCambioMoneda.getHistorialCambioMonedaAproximado(nombreColumna1, txtAprox1, nombreColumna2, txtAprox2);
+	}
+}
