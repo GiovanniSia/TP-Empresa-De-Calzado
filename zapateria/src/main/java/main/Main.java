74442b3d66bcca234ebb3cecafb6ad6695b75e4e
sebasx3601 <sebastianx3600@gmail.com>
2021-10-14 01:18:05 -0300

    Informe caja diaria recibio una mejora en su visualizacion ahoar es una pagina por subreporte

--- zapateria/src/main/java/main/Main.java
+++ zapateria/src/main/java/main/Main.java
@@ -20,10 +20,10 @@ public class Main {
 		ReControladorOperario co = new ReControladorOperario(new SucursalDTO(1, "FABRICA", "JOSE HERNANDEZ","123","BSAS","TORTGUITAS","ARG","1234","PAPU"));
 		co.inicializar();
 		*/
-		
-		ReporteFactura reporte = new ReporteFactura("A000011", 1);
+		/*
+		ReporteFactura reporte = new ReporteFactura("B000011", 1);
 		reporte.mostrar();
-		
+		*/
 		/*
 		ReporteCajaDiaria ca = new ReporteCajaDiaria(1);
 		ca.mostrarUltimoReporte();
