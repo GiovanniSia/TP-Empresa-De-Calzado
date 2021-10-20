cd6456eaa65163d8184aae82aeaecf717e9773a0
sebasx3601 <sebastianx3600@gmail.com>
2021-10-20 14:45:14 -0300

    Se actualizo el modelo de datos

--- zapateria/src/main/java/main/Main.java
+++ zapateria/src/main/java/main/Main.java
@@ -22,20 +22,26 @@ public class Main {
 				"ARG", "BS AS", "Tortuguitas", "1667");
 		HashMap<Integer,Integer> detalle = new HashMap<Integer,Integer>();
 		detalle.put(1, 5);
-		CompraVirtualDTO cvd = new CompraVirtualDTO(cliente,detalle,1,500);
+		//CompraVirtualDTO cvd = new CompraVirtualDTO(cliente,detalle,1,500);
+		CompraVirtualDTO cvd = new CompraVirtualDTO(detalle, 1, 500, 1, "Sebas",
+				"Cubilla", "123131231", "sebastianx3600@gmail.com", "Minorista", 
+				"RI", "Calle falsa", "5421", "Argentina",
+				"Buenos Aires", "Tortuguitas", "1667");
 		compras.add(cvd);
 		
 		detalle = new HashMap<Integer,Integer>();
 		detalle.put(1, 1);
 		detalle.put(2, 3);
-		CompraVirtualDTO cvd2 = new CompraVirtualDTO(cliente,detalle,2,3);
+		CompraVirtualDTO cvd2 = new CompraVirtualDTO(detalle, 1, 500, 1, "Sebas",
+				"Cubilla", "123131231", "sebastianx3600@gmail.com", "Minorista", 
+				"RI", "Calle falsa", "5421", "Argentina",
+				"Buenos Aires", "Tortuguitas", "1667");
 		compras.add(cvd2);
-		
 		JsonListaCompraVirtual.guardarLista(compras);
 		
 		compras = JsonListaCompraVirtual.getLista();
 		for(CompraVirtualDTO cvFOR: compras) {
-			System.out.println(cvFOR.getCliente().getIdCliente()+ ", "+cvFOR.getCliente().getNombre()+": "+cvFOR.getCompra().get(1));
+			System.out.println(cvFOR+ ", "+cvFOR.getNombre()+": "+cvFOR.getCompra().get(1));
 		}
 	}
 }
