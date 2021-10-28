7523e528e70109180e14fe2cafadea961f576e27
leonelSubelza <subelza150@gmail.com>
2021-10-28 17:40:51 -0300

    Se borra el proyecto aparte

--- Automatizacion/sql/scriptAgenda.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE `agenda`;
-USE agenda;
-CREATE TABLE `personas`
-(
-  `idPersona` int(11) NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`idPersona`)
-);
--- Automatizacion/src/main/java/presentacion/vista/TestSonar.js
+++
@@ -1,5 +0,0 @@
-
-
-const pi = "yes, please";
-pi = 3.14;  // Noncompliant
-
