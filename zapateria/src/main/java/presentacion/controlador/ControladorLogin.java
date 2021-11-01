c34980504eac1cffd2d11902dfed18d0dc2408b8
GiovanniSia <76817363+GiovanniSia@users.noreply.github.com>
2021-10-31 23:24:36 -0300

    Cambia la seleccion de sucursal por su objeto

--- zapateria/src/main/java/presentacion/controlador/ControladorLogin.java
+++ zapateria/src/main/java/presentacion/controlador/ControladorLogin.java
@@ -16,21 +16,21 @@ public class ControladorLogin {
 	private Controlador controlador;
 	private VentanaLogin ventanaLogin;
 	private Empleado empleado;
-	@SuppressWarnings("unused")
+
 	private EmpleadoDTO empleadoInicioSesion;
 	private List<SucursalDTO> sucursales;
 	private Sucursal sucursal;
-	
-	private String sucursalSeleccionada;
+
+	private SucursalDTO sucursalSeleccionada;
 
 	public ControladorLogin() {
 		this.controlador = new Controlador();
 		this.empleado = new Empleado(new DAOSQLFactory());
 		this.sucursal = new Sucursal(new DAOSQLFactory());
-	
+
 		this.ventanaLogin = new VentanaLogin();
 		this.sucursales = sucursal.readAll();
-		
+
 	}
 
 	public void inicializar() {
@@ -38,33 +38,43 @@ public class ControladorLogin {
 		rellenarCombobox();
 	}
 
-	@SuppressWarnings("unchecked")
 	public void rellenarCombobox() {
 		for (SucursalDTO s : sucursales) {
 			ventanaLogin.getCbSucursales().addItem(s.getNombre());
 		}
 	}
-	
-	
+
 	@SuppressWarnings({ "deprecation" })
 	public void validarUsuario(ActionEvent a) {
 		String correo = this.ventanaLogin.getTxtFieldCorreo().getText();
 		String contra = this.ventanaLogin.getTxtFieldContra().getText();
-		
-		if(empleado.selectUser(correo, contra)==null) {
+
+		if (empleado.selectUser(correo, contra) == null) {
 			JOptionPane.showMessageDialog(null, "Ingreso denegado, datos invalidos");
 			return;
 		}
-		
-		JOptionPane.showMessageDialog(null, "Ingreso exitoso, bienvenido "+ empleado.selectUser(correo, contra).getNombre());
-		
-		sucursalSeleccionada = this.ventanaLogin.getCbSucursales().getSelectedItem().toString();
+
+		JOptionPane.showMessageDialog(null,
+				"Ingreso exitoso, bienvenido " + empleado.selectUser(correo, contra).getNombre());
+
+		sucursalSeleccionada = obtenerSucursalSleccionada();
 		empleadoInicioSesion = empleado.selectUser(correo, contra);
-		
-//		System.out.println("Usuario Ingresado: " + empleado.selectUser(correo, contra).getCorreoElectronico() + " Sucursal: " + sucursalSeleccionada);
-		
+
+		System.out.println("Usuario Ingresado: " + empleado.selectUser(correo, contra).getCorreoElectronico() + " Sucursal: " + sucursalSeleccionada.getNombre());
+
 		ocultarVentanaLogin();
 		iniciarZapateria();
+	}
+
+	public SucursalDTO obtenerSucursalSleccionada() {
+		 String nombreSucursalSeleccionado = this.ventanaLogin.getCbSucursales().getSelectedItem().toString();
+		 SucursalDTO sucursalSeleccionada=null;	
+		 for (SucursalDTO s : sucursales) {
+				if(s.getNombre().equals(nombreSucursalSeleccionado)) {
+					sucursalSeleccionada = s;
+				}
+			}
+		return sucursalSeleccionada;
 	}
 
 	public void iniciarZapateria() {
