fcf11db3001a8acafd5a518bb961d623d84a664d
sebasx3601 <sebastianx3600@gmail.com>
2021-11-02 18:33:32 -0300

    Se puede ver y agregar los pasos

--- zapateria/src/main/java/presentacion/controlador/fabrica/receta/ControladorVerPasos.java
+++ zapateria/src/main/java/presentacion/controlador/fabrica/receta/ControladorVerPasos.java
@@ -1,15 +1,18 @@
 package presentacion.controlador.fabrica.receta;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-import dto.FabricacionesDTO;
 import dto.PasoDTO;
 import modelo.ModeloPaso;
 import persistencia.dao.mysql.DAOSQLFactory;
 import presentacion.vista.fabrica.receta.VerPasos;
 
-public class ControladorVerPasos {
+public class ControladorVerPasos implements ActionListener {
 	
 	VerPasos ventanaPrincipal;
 	ModeloPaso modeloPaso;
@@ -19,8 +22,22 @@ public class ControladorVerPasos {
 		ventanaPrincipal = new VerPasos();
 		modeloPaso = new ModeloPaso(new DAOSQLFactory());
 		pasosEnLista = new ArrayList<PasoDTO>();
+		
+		ventanaPrincipal.getTextDescripcion().addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				refrescarTabla();
+			}
+		});
+		ventanaPrincipal.getTextId().addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				refrescarTabla();
+			}
+		});
+		ventanaPrincipal.getBtnAgregar().addActionListener(r-> agregarPaso(r));
 	}
-	
+
 	public void inicializar() {
 		refrescarTabla();
 		ventanaPrincipal.mostrarVentana();
@@ -42,10 +59,17 @@ public class ControladorVerPasos {
 	private List<PasoDTO> recuperarListaPasos() {
 		List<PasoDTO> ret = new ArrayList<PasoDTO>();
 		List<PasoDTO> todosLosPasos = this.modeloPaso.readAll();
-		String idBuscar = this.ventanaPrincipal.getTextId()+"";
+		String idBuscar = this.ventanaPrincipal.getTextId().getText()+"";
 		String descripBuscar = this.ventanaPrincipal.getTextDescripcion().getText();
 		for(PasoDTO p: todosLosPasos) {
-			if((p.getIdPaso()+"").matches(".*"+idBuscar.toLowerCase()+".*") && p.getDescripcion().toLowerCase().matches(".*"+descripBuscar.toLowerCase()+".*")) {
+			boolean deboAgregar = true;
+			if(!idBuscar.equals("")) {
+				deboAgregar = deboAgregar && (p.getIdPaso()+"").matches(".*"+idBuscar.toLowerCase()+".*");
+			}
+			if(!descripBuscar.equals("")) {
+				deboAgregar = deboAgregar && p.getDescripcion().toLowerCase().matches(".*"+descripBuscar.toLowerCase()+".*");
+			}
+			if(deboAgregar) {
 				ret.add(p);
 			}
 		}
@@ -56,6 +80,26 @@ public class ControladorVerPasos {
 		ventanaPrincipal.getModelPasos().setRowCount(0);
 		ventanaPrincipal.getModelPasos().setColumnCount(0);
 		ventanaPrincipal.getModelPasos().setColumnIdentifiers(ventanaPrincipal.getNombreColumnas());
+	}
+	
+	private void agregarPaso(ActionEvent r) {
+		System.out.println("Entro en el boton");
+		String pasoAgregar = this.ventanaPrincipal.getTextDescripcionAgregar().getText();
+		if(pasoAgregar == null) {
+			return;
+		}
+		if(pasoAgregar.equals("")) {
+			return;
+		}
+		if(pasoAgregar.length()>20) {
+			return;
+		}
+		this.modeloPaso.insert(new PasoDTO(0,pasoAgregar));
+		refrescarTabla();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 	}
 
 	public static void main(String[] args) {
