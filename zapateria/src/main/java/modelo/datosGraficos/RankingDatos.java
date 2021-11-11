1fcb66e8c928117fe03fba17658267e7e24c20e7
sebasx3601 <sebastianx3600@gmail.com>
2021-11-10 21:47:43 -0300

    Se puede ver ranking por vendedor, pero falta limitar el top x mejores vendedores

--- zapateria/src/main/java/modelo/datosGraficos/RankingDatos.java
+++ zapateria/src/main/java/modelo/datosGraficos/RankingDatos.java
@@ -3,8 +3,12 @@ package modelo.datosGraficos;
 import java.util.Date;
 import java.util.HashMap;
 
+import dto.EmpleadoDTO;
+import dto.FacturaDTO;
 import dto.IngresosDTO;
 import dto.SucursalDTO;
+import modelo.Empleado;
+import modelo.Factura;
 import modelo.Ingresos;
 import modelo.Sucursal;
 import persistencia.dao.mysql.DAOSQLFactory;
@@ -15,12 +19,37 @@ public class RankingDatos {
 		HashMap<SucursalDTO,Double> ret = new HashMap<SucursalDTO,Double>();
 		Sucursal modeloSucursal = new Sucursal(new DAOSQLFactory());
 		for(SucursalDTO s: modeloSucursal.readAll()) {
-			ret.put(s, getIngresos(s.getIdSucursal(), diasParaAtras));
+			ret.put(s, getIngresosPorSucursal(s.getIdSucursal(), diasParaAtras));
 		}
 		return getTop(ret, cantidadSucursalesDeseada);
 	}
 	
-	static private Double getIngresos(int idSucursal, int diasParaAtras) {
+	static public HashMap<EmpleadoDTO, Double> getRankingVentasxVendedor(int cantidadEmpleadosDeseado, int diasParaAtras){
+		HashMap<EmpleadoDTO,Double> ret = new HashMap<EmpleadoDTO,Double>();
+		Empleado modeloEmpleado = new Empleado(new DAOSQLFactory());
+		for(EmpleadoDTO e: modeloEmpleado.readAll()) {
+			ret.put(e, getIngresosPorEmpleado(e.getIdEmpleado(), diasParaAtras));
+			System.out.println(ret.get(e)+" resultado final");
+		}
+		return ret;
+	}
+	
+	static private Double getIngresosPorEmpleado(int idEmpleado, int diasParaAtras) {
+		Double ret = 0.0;
+		Factura modeloFactura = new Factura(new DAOSQLFactory());
+		for(FacturaDTO i: modeloFactura.readAll()) {
+			if(i.getIdVendedor() == idEmpleado) {
+				if(fechaEsValida(i,diasParaAtras)) {
+					System.out.println("Se sumo");
+					ret += i.getTotalFactura();
+					System.out.println(ret);
+				}
+			}
+		}
+		return ret;
+	}
+	
+	static private Double getIngresosPorSucursal(int idSucursal, int diasParaAtras) {
 		Double ret = 0.0;
 		Ingresos modeloIngreso = new Ingresos(new DAOSQLFactory());
 		for(IngresosDTO i: modeloIngreso.readAll()) {
@@ -35,6 +64,23 @@ public class RankingDatos {
 	
 	@SuppressWarnings("deprecation")
 	private static boolean fechaEsValida(IngresosDTO i, int diasParaAtras) {
+		Date fechaComparar = new Date();
+		Date fechaIngreso = new Date();
+		String[] fechaIngregoString = i.getFecha().split("-"); 
+		fechaIngreso.setYear(Integer.valueOf(fechaIngregoString[0])-1900);
+		fechaIngreso.setMonth(Integer.valueOf(fechaIngregoString[1])-1);
+		fechaIngreso.setDate(Integer.valueOf(fechaIngregoString[2]));
+		
+		fechaComparar.setDate(Integer.valueOf(fechaComparar.getDate()-diasParaAtras));
+		//fechaComparar.setDate(1);
+		if(fechaComparar.compareTo(fechaIngreso)>0) {
+			return false;
+		}
+		return true;
+	}
+	
+	@SuppressWarnings("deprecation")
+	private static boolean fechaEsValida(FacturaDTO i, int diasParaAtras) {
 		Date fechaComparar = new Date();
 		Date fechaIngreso = new Date();
 		String[] fechaIngregoString = i.getFecha().split("-"); 
@@ -83,6 +129,11 @@ public class RankingDatos {
 		HashMap<SucursalDTO,Double> rankingSucursal = getRankingVentasXSucursales(3,1);
 		for(SucursalDTO s: rankingSucursal.keySet()) {
 			System.out.println(s.getNombre()+": "+rankingSucursal.get(s));
+		}
+		
+		HashMap<EmpleadoDTO, Double> rankingVendedor = getRankingVentasxVendedor(2,0);
+		for(EmpleadoDTO e: rankingVendedor.keySet()) {
+			System.out.println(e.getNombre()+": "+rankingVendedor.get(e));
 		}
 	}
 
